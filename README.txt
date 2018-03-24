26adcc687d78ffff7b088c358a10eb6b7a56c087
Kim <kimberly.radford@eagles.ewu.edu>
2018-03-23 22:03:19 -0700

    Redo Sperm Test Website
    Redo of Sperm Test Website for Final

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/kmradford/pen/YeBpzy.
+A Pen created at CodePen.io. You can find this one at https://codepen.io/kmradford/pen/qoXmbL.
 
  
--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+$(document).ready(function() {
+  $('.nav button').click(function() {
+    $('.nav ul').toggleClass('active')
+  })
+  $('.nav .nav-links a').click(function() {
+    $('.nav ul').removeClass('active')
+  })
+})
+
 $('div').tubular({
  videoId : '9DNIXolDEME',
  mute: false,
